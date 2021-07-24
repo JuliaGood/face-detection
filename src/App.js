70c4932a1129be846a2c466417b2d43e76bd41e7
JuliaGood <gudak.yuliya@gmail.com>
2021-07-24 23:41:48 +0300

    added first files into gitHub

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Logo from './components/logo/Logo';
+import ImageLinkInput from './components/imageLinkInput/ImageLinkInput';
+
+function App() {
+  return (
+    <div className="App">
+      <Logo/>
+      <ImageLinkInput/>
+    </div>
+  );
+}
+
+export default App;
